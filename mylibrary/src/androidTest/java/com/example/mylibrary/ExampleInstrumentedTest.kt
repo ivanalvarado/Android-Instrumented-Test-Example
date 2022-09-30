e99247950765393a02bf7ee6698aca4e811d5480
Ivan Alvarado <ivalvara21@gmail.com>
2022-09-29 21:22:54 -0700

    Init commit

---
+++ mylibrary/src/androidTest/java/com/example/mylibrary/ExampleInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package com.example.mylibrary
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MyLibraryActivity::class.java)
+
+    @Test
+    fun test_myLibraryActivity() {
+        onView(withId(R.id.my_library_hello_world_text)).check(matches(withText(R.string.hello_world)))
+    }
+}
